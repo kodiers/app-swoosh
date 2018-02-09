47bcc1f94f9302336f17a175b20210de73fbe86b
Victor Yamchinov <kodiers@gmail.com>
2018-02-09 03:46:16 +0300

    added layout and segue

--- app-swoosh/app-swoosh/ViewController.swift
+++ app-swoosh/app-swoosh/ViewController.swift
@@ -25,7 +25,9 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func unwindStoryBoardSegue(unwindSegue: UIStoryboardSegue) {
+        
+    }
 }
 
